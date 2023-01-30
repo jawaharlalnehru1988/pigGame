7971942ae89f6b494ed9364cffb6c8af5aacbf8f
jawaharlalnehru1988 <nehru00123@gmail.com>
2023-01-30 07:52:50 +0530

    dice is added

--- script.js
+++ script.js
@@ -4,8 +4,20 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-console.log(diceEl.classList.add('hidden'));
+diceEl.classList.add('hidden');
+console.log(diceEl.src);
+btnRoll.addEventListener('click', function(){
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random()*6)+1;
+
+    //2.Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+})
